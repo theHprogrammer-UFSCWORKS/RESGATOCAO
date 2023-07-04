b8ebd848d37e25dfe4665bedf19569e06890c1dd
LuccaMS <saintangel961@gmail.com>
2023-07-04 13:38:18 -0300

    teste

--- resgatocao_dsw/settings.py
+++ resgatocao_dsw/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+'''STATIC_URL = '/static/'
 STATIC_ROOT= '/home/saintangel/RESGATOCAO/static'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL = 'media/'
@@ -137,7 +137,17 @@ if DEBUG:
             os.path.join(BASE_DIR, 'static')
        ]
 else:
-     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+     STATIC_ROOT = os.path.join(BASE_DIR, 'static')'''
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/saintangel/RESGATOCAO/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
